fb7bc9caf2e9dd6f68de7bccdb45507ab0c34254
Kirill <syskirill.com>
2024-10-12 11:25:13 +0300

    Initial Commit

---
+++ TestDelegateAppTests/TestDelegateAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestDelegateAppTests.swift
+//  TestDelegateAppTests
+//
+//  Created by Кирилл Сысоев on 12.10.24.
+//
+
+import Testing
+@testable import TestDelegateApp
+
+struct TestDelegateAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
